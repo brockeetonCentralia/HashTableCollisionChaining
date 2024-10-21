1895e6700e35460e8b928a87d85a1951c46961ec
brockeeton <brockeeton@CC-BSY58V3>
2024-10-21 11:50:13 -0700

    Add project files.

---
+++ HashTableCollisionChaining/HashTable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableCollisionChaining
+{
+    internal class HashTable<TKey, TValue>
+    {
+        private List<KeyValuePair<TKey, TValue>>[] buckets;
+        private int size;
+
+        public HashTable(int size)
+        {
+            this.size = size;
+            buckets = new List<KeyValuePair<TKey, TValue>>[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                buckets[i] = new List<KeyValuePair<TKey, TValue>>();
+            }
+        }
+
+        private int GetHash(TKey key)
+        {
+            return key.GetHashCode() % size;
+        }
+
+        public void Insert(TKey key, TValue value)
+        {
+            int hash = GetHash(key);
+            buckets[hash].Add(new KeyValuePair<TKey, TValue>(key, value));
+        }
+        public void Display()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("Bucket " + i + ": ");
+                foreach (var pair in buckets[i])
+                {
+                    Console.Write($"[{pair.Key}, {pair.Value}] ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ HashTableCollisionChaining/Program.cs
@@ -0,0 +1,9 @@
+﻿using HashTableCollisionChaining;
+
+HashTable<int, string> hashTable = new HashTable<int, string>(5);
+
+hashTable.Insert(1, "One");
+hashTable.Insert(6, "Six"); 
+hashTable.Insert(11, "Eleven");
+
+hashTable.Display();
